c8e85df2a7f7c4d1a7db04742c9231bdc339b4b4
Khazov Anton <ulowpie@gmail.com>
2021-04-20 22:01:50 +0500

    Delete encrypt.py

--- replacement/encrypt.py
+++
@@ -1,9 +0,0 @@
-#Функция шифрования по ключу
-#text - list символов текста
-#key - list с перестановкой на алфавите
-def encrypt(text, key):
-    result = []
-    for i in range(len(text)):
-        result.append(key[alphabet.index(text[i])])
-
-    return result
